20285847a0f2e57657b88c61fe199515e7c62399
Rishabh Rawat <www.rishabhrawat570@gmail.com>
2018-04-11 00:36:27 +0530

    Add files via upload

---
+++ static/js/list.js
@@ -0,0 +1,54 @@
+$(document).ready(() => {
+
+    $('.delete').mouseenter(function () {
+        $(this).css('cursor', 'pointer')
+        $(this).children().fadeIn()
+    })
+    $('.delete').mouseleave(function () {
+        $(this).children().fadeOut()
+    })
+
+
+    // Upvote handler
+    $('.upvotes').click(function(e) {
+        e.preventDefault();
+        var refer = $(this)
+        var filter = $(this).attr("id")
+
+        $.get('views/upvote/', mydata={ 'filter': filter }, function (votes) {
+            refer.parent().next().html(votes);
+        })
+        .fail(function () {
+            $('.fail').fadeIn(1000, 'swing')
+        })
+    })
+
+    // Downvote handler
+    $('.downvotes').click(function(e) {
+        e.preventDefault();
+        var refer_down = $(this)
+        var filter = $(this).attr("id")
+
+        $.get('views/downvote/', mydata={ 'filter': filter }, function (votes) {
+            refer_down.parent().prev().html(votes);
+        })
+        .fail(function () {
+            $('.fail').css('display', 'block')
+        })
+        
+    })
+
+    $('.delete').click(function(e) {
+        e.preventDefault();
+        var refer = $(this)
+        var post_id = $(this).attr('id')
+        $.get('views/delete/', mydata = {'post_id': post_id}, function(callback) {
+            refer.parent().css('display', 'none')
+            console.log(callback)
+        })
+        .fail(function (callback) {
+            refer.parent().html(callback)
+        })
+    })
+
+})
